0b847461bb4b8e036a22d1cdee98b6790fda1293
hexosse <hexosse@gmail.com>
2019-12-06 10:44:51 +0100

    renamed user env var

--- alpine-base/rootfs/etc/entrypoint.d/001-base.sh
+++ alpine-base/rootfs/etc/entrypoint.d/001-base.sh
@@ -11,10 +11,10 @@ env_set "LOG_LEVEL" $(LOG_LEVEL_VALUE ${DEBUG_LEVEL})
 LOG info "Debug level set to $(LOG_LEVEL_NAME ${LOG_LEVEL})"
 
 # Envs
-env_set DBX_USER "dsuite"
-env_set DBX_GROUP "dsuite"
-env_set DBX_UID "1000"
-env_set DBX_GID "1000"
+env_set DST_USER "dsuite"
+env_set DST_GROUP "dsuite"
+env_set DST_UID "1000"
+env_set DST_GID "1000"
 NEW_UID=$(env_get "NEW_UID")
 NEW_GID=$(env_get "NEW_GID")
 
@@ -22,9 +22,9 @@ NEW_GID=$(env_get "NEW_GID")
 env_reload
 
 ## Default User/Group
-[ -z "$(getent group "$DBX_GID")" ] && addgroup -S -g "${DBX_GID}" "${DBX_GROUP}"
-[ -z "$(getent passwd "$DBX_UID")" ] && adduser -S -u "${DBX_UID}" -G "${DBX_GROUP}" -s /bin/sh "${DBX_USER}"
+[ -z "$(getent group "$DST_GID")" ] && addgroup -S -g "${DST_GID}" "${DST_GROUP}"
+[ -z "$(getent passwd "$DST_UID")" ] && adduser -S -u "${DST_UID}" -G "${DST_GROUP}" -s /bin/sh "${DST_USER}"
 
 # Modify User/Group
-[ -n "${NEW_UID}" ] && set_uid "${NEW_UID}" "${DBX_USER}" "/home/${DBX_USER}"
-[ -n "${NEW_GID}" ] && set_gid "${NEW_GID}" "${DBX_GROUP}" "/home/${DBX_USER}"
+[ -n "${NEW_UID}" ] && set_uid "${NEW_UID}" "${DST_USER}" "/home/${DST_USER}"
+[ -n "${NEW_GID}" ] && set_gid "${NEW_GID}" "${DST_GROUP}" "/home/${DST_USER}"
