54c75a878f9e7cf48838f167b872da0786dbca66
hexosse <hexosse@gmail.com>
2020-01-17 12:47:09 +0100

    Add more cron period

--- alpine-base/install-base.sh
+++ alpine-base/install-base.sh
@@ -38,6 +38,52 @@ fi
 [ "$(find /usr/local/bin -type f  | wc -l)" -gt "0" ] && chmod 0755 /usr/local/bin/*
 [ "$(find /usr/local/sbin -type f | wc -l)" -gt "0" ] && chmod 0755 /usr/local/sbin/*
 
+# Add more cron period
+chmod 600 /etc/crontabs/root
+mkdir -p /etc/periodic/daily_1am
+mkdir -p /etc/periodic/daily_2am
+mkdir -p /etc/periodic/daily_3am
+mkdir -p /etc/periodic/daily_4am
+mkdir -p /etc/periodic/daily_5am
+mkdir -p /etc/periodic/daily_6am
+mkdir -p /etc/periodic/daily_7am
+mkdir -p /etc/periodic/daily_8am
+mkdir -p /etc/periodic/daily_9am
+mkdir -p /etc/periodic/daily_10am
+mkdir -p /etc/periodic/daily_11am
+mkdir -p /etc/periodic/daily_12am
+mkdir -p /etc/periodic/daily_1pm
+mkdir -p /etc/periodic/daily_2pm
+mkdir -p /etc/periodic/daily_3pm
+mkdir -p /etc/periodic/daily_4pm
+mkdir -p /etc/periodic/daily_5pm
+mkdir -p /etc/periodic/daily_6pm
+mkdir -p /etc/periodic/daily_7pm
+mkdir -p /etc/periodic/daily_8pm
+mkdir -p /etc/periodic/daily_9pm
+mkdir -p /etc/periodic/daily_10pm
+mkdir -p /etc/periodic/daily_11pm
+mkdir -p /etc/periodic/daily_12pm
+mkdir -p /etc/periodic/weekly_sunday
+mkdir -p /etc/periodic/weekly_monday
+mkdir -p /etc/periodic/weekly_tuesday
+mkdir -p /etc/periodic/weekly_wednesday
+mkdir -p /etc/periodic/weekly_thursday
+mkdir -p /etc/periodic/weekly_friday
+mkdir -p /etc/periodic/weekly_saturday
+mkdir -p /etc/periodic/monthly_january
+mkdir -p /etc/periodic/monthly_february
+mkdir -p /etc/periodic/monthly_march
+mkdir -p /etc/periodic/monthly_april
+mkdir -p /etc/periodic/monthly_may
+mkdir -p /etc/periodic/monthly_june
+mkdir -p /etc/periodic/monthly_july
+mkdir -p /etc/periodic/monthly_august
+mkdir -p /etc/periodic/monthly_september
+mkdir -p /etc/periodic/monthly_october
+mkdir -p /etc/periodic/monthly_november
+mkdir -p /etc/periodic/monthly_december
+
 # Make startup scripts accessible and executable
 [ -d /startup.d ]   && [ "$(find /startup.d -name '*.sh' -type f | wc -l)" -gt "0" ]   && chmod 0755 /startup.d/*.sh
 [ -d /startup.1.d ] && [ "$(find /startup.1.d -name '*.sh' -type f | wc -l)" -gt "0" ] && chmod 0755 /startup.1.d/*.sh
