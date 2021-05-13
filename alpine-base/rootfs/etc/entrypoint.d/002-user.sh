5327cc8539156725692ded3e9d07855f587eec83
hexosse <hexosse@gmail.com>
2021-05-13 15:51:30 +0200

    No more dsuite user by default

---
+++ alpine-base/rootfs/etc/entrypoint.d/002-user.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+
+## User which will be running the main process
+USER=$(env_get "USER")
+
+## Auto load /etc/profile for all known users
+echo "source /etc/profile" > /root/.ashrc
+echo "source /etc/profile" > /root/.bashrc
+([[ -d "$HOME"  ]] && echo "source /etc/profile" > "$HOME/.ashrc") || true
+([[ -d "$HOME"  ]] && echo "source /etc/profile" > "$HOME/.bashrc") || true
+([[ -d "$(getent passwd "$USER" | cut -d: -f6)"  ]] && echo "source /etc/profile" > "$(getent passwd "$USER" | cut -d: -f6)/.ashrc") || true
+([[ -d "$(getent passwd "$USER" | cut -d: -f6)"  ]] && echo "source /etc/profile" > "$(getent passwd "$USER" | cut -d: -f6)/.bashrc") || true
+
+## direct sourcing in case if the user do not have a home folder
+source /etc/profile
